ca6f4736b82f4e90de85806a0d3a19508479ae4b
BobbyJoeSmith3 <lakota.sapa@gmail.com>
2018-09-28 23:32:56 -0400

    Unresolved: dynamically add css to each image that has been prepended to page

--- js/script.js
+++ js/script.js
@@ -51,7 +51,28 @@ function populateImages(dir_id) {
   var play_dir = ['_Aleph, Infinitely Veritable Typeface, Nick Adam, 2018 Alum.png', '122 Brook to 41 Sheldon, Elena Foraker, 2020.png', 'Ampersands, Olivia Orr, 2020.png', 'Atypical Archetypes, Lake Buckley, 2017 Alum.png', 'Boxing Dada, Maria Rull Bescos, 2019 Alum.png', 'Doodles Turned Digital, Etienne Adams, 2021.png', 'Green, Hirosi Yoshimura, 2019.png', 'The Plot Thins, Zoe Schneider, 2019.png', 'UselessMachine_KanyaratLoetaphirom_2019.png', 'Zoo’s Inner Voices, Zoo Somaini, 2019.png'];
   for (let i = 0; i < play_dir.length; i++) {
     $(".image-confetti").prepend(`<img alt="images" src="${filepath}${play_dir[i]}">`);
+    // Add CSS to newly created image node
+    // To keep from creating a ridiculous number of classes, use first child selector. Since we are prepending the images, the image we just created will always be the first child
+    // Calculated as %
+    let left = randomIntFromInterval(10, 90);
+    // Calculated as pixels
+    let width = randomIntFromInterval(100, 200);
+    $(".image-confetti:first-child").css({
+      "display":  "block",
+      "position": "absolute",
+      "top": "-150px",
+      "left": `${left}%`,
+      "width": `${width}px`,
+      "margin":"-60px 0 0 -60px",
+      "-webkit-animation":"spin 2s linear infinite",
+      "-moz-animation":"spin 2s linear infinite",
+      "animation":"spin 2s linear infinite"
+    })
   }
+}
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 $(".image-confetti img").draggable();
